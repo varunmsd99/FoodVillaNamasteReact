2854ba877cb6a039342e98ff99aa71f0d797ad7f
varunmsd99 <varunpelluru@gmail.com>
2023-04-23 14:22:00 +0530

    230423 commit

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -2,15 +2,12 @@ import { RestaurantList } from "../Constant";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
-
   return (
-      <div className="Restaurant-list" id="all-restaurants">
-        {RestaurantList.map((restaurant) => {
-          return (
-            <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
-          );
-        })}
-      </div>
+    <div className="Restaurant-list">
+      {RestaurantList.map((restaurant) => {
+        return <RestaurantCard {...restaurant.data} key={restaurant.data.id} />;
+      })}
+    </div>
   );
 };
 export default Body;
