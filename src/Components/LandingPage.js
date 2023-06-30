71cce4453e966368b2bec352845dc123c9761cc8
varunmsd99 <varunpelluru@gmail.com>
2023-06-30 23:49:20 +0530

    300623

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,12 +1,11 @@
-import FoodVillaTempImg from "../Images/FoodVillaTempLandingPage.png"
+import FoodVillaTempImg from "../Images/FoodVillaTempLandingPage.png";
 
 const LandingPage = () => {
   return (
     <div className="landing-page-container">
       <img src={FoodVillaTempImg}/>
       <h1 className="heading">We're Cooking Something Delicious!</h1>
-      <p className="sub-heading">Our website is currently under construction.</p>
-      <p className="message">Come back soon to experience the taste of perfection.</p>
+      <p className="message">Our website is currently under construction. Come back soon to experience the taste of perfection.</p>
       <div className="button-wrapper">
         <a href="/" className="button">Go Back to Homepage</a>
       </div>
--- src/Components/MenuCategory.js
+++ src/Components/MenuCategory.js
@@ -1,12 +1,22 @@
 import MenuItem from "./MenuItem";
+import { useState } from "react";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const MenuCategory = ({title, itemCards}) => {
   const menuData = itemCards.map((x) => {return x.card.info});
+  const [isOpen, setIsOpen] = useState(true);
+  const toggleCategory = () => {
+    setIsOpen(!isOpen);
+  }
   return (
-    <div className="menu-category">
-        <h3>{title} ({itemCards.length})</h3>
+    <div className={`menu-category ${isOpen ? 'active' : ''}`}  onClick={toggleCategory}>
+        <div className="menu-category-header">
+          <h3>{title} ({itemCards.length})</h3>
+          <div className={`menu-chevron-down ${isOpen ? 'active' : ''}`}>{<FontAwesomeIcon icon={faChevronDown} />}</div>
+        </div>
         <div className="menu-item">
-        {menuData.map((obj) => {
+        {isOpen && menuData.map((obj) => {
           return <MenuItem {...obj} key={{...obj}.id} />
         })} 
         </div>
