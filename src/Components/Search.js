8f97b7b8774b7378b06d7d6988dc5927b0c77564
Varun Kumar <80061365+varunmsd99@users.noreply.github.com>
2023-12-25 12:12:53 +0530

    Update Search.js

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -6,6 +6,7 @@ import useRestaurantsData from "../Hooks/UseRestaurantsData";
 import usePopularCuisinesData from "../Hooks/usePopularCuisinesData";
 import { IMG_CDN_URL } from "../Helpers/Constant";
 import RestaurantSearchCard from "./RestaurantSearchCard";
+import Home from ".Home"
 
 const Search = () => { 
   const Data = useRestaurantsData();
