fa78b999eb219faa9e98a35f7a58b5235d5195d6
varunmsd99 <varunpelluru@gmail.com>
2024-01-01 18:43:12 +0530

    optionalChaining update varun

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { faLinkedin, faGithub, faDiscord, faXTwitter } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
