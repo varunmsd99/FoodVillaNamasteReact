7f9523746480e4a9e70170baaa5070f8032d5759
varunmsd99 <varunpelluru@gmail.com>
2024-01-07 00:40:21 +0530

    orientation update varun

--- src/Components/Orientation.js
+++ src/Components/Orientation.js
@@ -4,7 +4,7 @@ import landscape from "../Images/Landscape.webp"
 const Orientation = () => {
   return (
     <div className="h-screen flex flex-col justify-center items-center">
-        <img src={landscape} className="rotate-90 h-[50vh] p-10 object- mx-auto"/>
+        <img src={landscape} className="rotate-90 h-[50vh] p-10 object-cover mx-auto"/>
         <div className="text-[#60606e] mt-10 text-center mx-10">
             <h2 className="text-lg font-bold">Rotate your device</h2>
             <h3 className="text-sm">We do not support landscape mode yet. Please go back to portrait mode for the best experience</h3>
