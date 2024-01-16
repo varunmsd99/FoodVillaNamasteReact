7305a4a6740d4348185e089e4a8f6a3b7fb73819
varunmsd99 <varunpelluru@gmail.com>
2024-01-16 15:20:36 +0530

    cart update varun

--- src/Components/Orientation.js
+++ src/Components/Orientation.js
@@ -8,7 +8,7 @@ const Orientation = () => {
         <div className="text-[#60606e] mt-10 text-center mx-10">
             <h2 className="text-lg font-bold">Rotate your device</h2>
             <h3 className="text-sm">We do not support landscape mode yet.</h3>
-            <h2 className="text-sm text-red-500">For best experience, Please switch to <strong>Landscape mode</strong> and enable <strong>Desktop mode</strong></h2>
+            <h2 className="text-sm text-red-500">For best experience, Please switch to <strong>Landscape</strong> and enable <strong>Desktop mode</strong></h2>
         </div>
     </div>
   )
