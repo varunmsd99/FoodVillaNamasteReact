1cd5c2408c47fc0f4d847db4290438e592143878
varunmsd99 <varunpelluru@gmail.com>
2024-04-20 01:04:06 +0530

    19Apr CORS update

--- src/Helpers/Constant.js
+++ src/Helpers/Constant.js
@@ -3,7 +3,7 @@ export const IMG_CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/
 export const WOYM_CARD_IMG_CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_288,h_360/";
 export const RES_CARD_IMG_CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 export const RES_CARD_IMG_CDN_URL_GREY = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660,e_grayscale/";
-export const REST_API_MENU_URL = (lat, lng) => {return `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=${lat}&lng=${lat}&restaurantId=`};
+export const REST_API_MENU_URL = (lat, lng) => {return `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=${lat}&lng=${lng}&restaurantId=`};
 export const CAROUSEL_CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_850,h_504/";
 export const GET_LOCATION_API_URL = "https://india-pincode-with-latitude-and-longitude.p.rapidapi.com/api/v1/pincode/";
 export const apiKey = process.env.LOCATION_API_KEY;
